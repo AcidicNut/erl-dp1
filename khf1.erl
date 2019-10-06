62886b7a9a76530c5e499ffdbf2ed3060a2fbb8c
acidicnut <lorinczb96@gmail.com>
2019-10-06 23:16:47 +0200

    done

--- khf1.erl
+++ khf1.erl
@@ -1,8 +1,8 @@
 -module(khf1).
 -author('email@unit.org.hu').
 -vsn('year-mm-dd').
-%-export([feldarabolasa/2]).
--compile(export_all).
+-export([feldarabolasa/2]).
+%-compile(export_all).
 
 sorok(M, Eleje, _R, Acc) when (Eleje > length(M)) -> lists:reverse(Acc);
 sorok(M, Eleje, R, Acc) -> sorok(M, Eleje + R, R, [lists:sublist(M, Eleje, R)|Acc]).
