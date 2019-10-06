bf08032ed1d2cbad206e67422d1dc8e9a79e2907
acidicnut <lorinczb96@gmail.com>
2019-10-06 23:16:21 +0200

    oszlopbontas + done

--- khf1.erl
+++ khf1.erl
@@ -7,4 +7,17 @@
 sorok(M, Eleje, _R, Acc) when (Eleje > length(M)) -> lists:reverse(Acc);
 sorok(M, Eleje, R, Acc) -> sorok(M, Eleje + R, R, [lists:sublist(M, Eleje, R)|Acc]).
 
-feldarabolasa(M, {R, C}) -> 1.
+elsohossz(L) -> length(hd(L)).
+
+oszlopok(L, C) -> oszlopBonto(L, 1, C, elsohossz(L),[]).
+
+oszlopBonto(_L, Eleje, _C, Elsohossz, Acc) when (Eleje > Elsohossz) -> lists:reverse(Acc);
+oszlopBonto(L, Eleje, C, Elsohossz, Acc) -> oszlopBonto(L, Eleje + C, C, Elsohossz, [oszlop(L, Eleje, Eleje + C, []) | Acc]).
+
+oszlop(L, _Elso, _Utolso, Acc) when (length(L) < 1) -> Acc;
+oszlop(L, Elso, Utolso, Acc) -> oszlop(tl(L), Elso, Utolso, lists:append(Acc, lists:sublist(hd(L), Elso, Utolso-Elso))).
+
+kisMatrixok(M, _C, Acc) when (length(M) < 1) -> Acc;
+kisMatrixok(M, C, Acc) -> 	kisMatrixok(tl(M), C, lists:append(Acc, oszlopok(hd(M), C))).
+
+feldarabolasa(M, {R, C}) -> kisMatrixok(sorok(M, 1, R, []), C, []).
