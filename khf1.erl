36ef85ae15dce493c464cc64d04cd4e71d74fff8
acidicnut <lorinczb96@gmail.com>
2019-10-06 22:34:37 +0200

    sorbontas

--- khf1.erl
+++ khf1.erl
@@ -3,3 +3,8 @@
 -vsn('year-mm-dd').
 %-export([feldarabolasa/2]).
 -compile(export_all).
+
+sorok(M, Eleje, _R, Acc) when (Eleje > length(M)) -> lists:reverse(Acc);
+sorok(M, Eleje, R, Acc) -> sorok(M, Eleje + R, R, [lists:sublist(M, Eleje, R)|Acc]).
+
+feldarabolasa(M, {R, C}) -> 1.
